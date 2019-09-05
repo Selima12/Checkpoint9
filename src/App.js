6e9d81b5e01577d14addaca745114c2f99b56cdf
Selima12 <selimahajslimene0@gmail.com>
2019-09-05 09:47:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default class App extends React.Component{
+  state={
+    text : '',
+    array: ['tomato','potato']
+  }
+
+  StoreValue =(val)=>{
+    this.setState({text : val.target.value})
+  }
+  PushToArray = ()=>{
+    this.state.array.push(this.state.text)
+   
+    this.setState({text : ''})
+
+  }
+  Effacer = (index)=>{
+    // console.log(index.target.id)
+    document.getElementById(index.target.id).style.textDecoration = "line-through"
+  }
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={this.state.text} onChange={this.StoreValue}/>
+  <button onClick={this.PushToArray}>Add</button>
+  <ul>
+    {
+      this.state.array.map((val,index)=><li id={index} onClick={this.Effacer.bind(index)}>{val}</li>)
+    }
+  </ul>
     </div>
   );
 }
+}
 
-export default App;
+
